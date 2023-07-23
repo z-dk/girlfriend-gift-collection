3fd3e11f188658f72da8dd4f5ae28f3c17fa2675
zdk <zhudengkui0000@126.com>
2023-07-23 17:16:15 +0800

    [0813]第一版

--- birth-want-to-say/config.js
+++ birth-want-to-say/config.js
@@ -1,22 +1,15 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的姑娘",  // 同上...
+        "送给我",
+        "心爱的姑娘",
         "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日！",
-        "这两年来，我见过你",
-        "奇怪的样子",
-        "可爱的样子",
-        "生气的样子",
-        "都是我喜欢的样子",
-        "对了，礼物收全了吗？",
-        "今年要吃好好的",
+        "这是我们相识后你的",
+        "第一个生日！",
+        "要吃好好的",
         "喂饱饱的",
-        "然后才有力气干活",
+        "然后才有力气学习",
         "和想我！",
         "生日快乐~~",
     ],
@@ -31,28 +24,22 @@ var config = {
      */
     imgs: {
         "送给我": "./imgs/songxinxin.jpeg",
-        "心爱的姑娘": "./imgs/nothing.jpg",
+        "心爱的姑娘": "./imgs/chirou.jpg",
         "今天是你的生日": "./imgs/shengri.jpeg",
-        "这是我们在一起的": "./imgs/zaiyiqi.png",
-        "第三个生日！": "./imgs/nothing.jpg",
-        "这两年来，我见过你": "./imgs/yangzi.jpeg",
-        "奇怪的样子": "./imgs/nothing.jpg",
-        "可爱的样子": "./imgs/nothing.jpg",
-        "生气的样子": "./imgs/nothing.jpg",
-        "都是我喜欢的样子": "./imgs/nothing.jpg",
-        "对了，礼物收全了吗？": "./imgs/chuo.gif",
-        "今年要吃好好的": "./imgs/chihaodian.jpeg",
+        "这是我们相识后你的": "./imgs/together.jpg",
+        "第一个生日！": "./imgs/dangao.png",
+        "要吃好好的": "./imgs/eatAndDrink.jpeg",
         "喂饱饱的": "./imgs/weibao.jpeg",
-        "然后才有力气干活": "./imgs/nothing.jpg",
-        "和想我！": "./imgs/nothing.jpg",
-        "生日快乐~~": "./imgs/nothing.jpg"
+        "然后才有力气学习": "./imgs/studying.gif",
+        "和想我！": "./imgs/biubiu.png",
+        "生日快乐~~": "./imgs/happybirthday.jpg"
     },
     
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "点我开始",
         play: "上音乐",
-        bannar_coming: "搞点颜色",
+        bannar_coming: "继续",
         balloons_flying: "好像还少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
@@ -61,5 +48,5 @@ var config = {
     },
 
     // 结束语
-    loveText: '爱你的X先生~'
+    loveText: '^_^'
 };
